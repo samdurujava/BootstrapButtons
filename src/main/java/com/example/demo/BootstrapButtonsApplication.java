cd38afce61b2f4fe2192e0247071d4339d94c947
GBTC441007ur <samduru1@gmail.com>
2019-07-15 09:49:20 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/BootstrapButtonsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapButtonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrapButtonsApplication.class, args);
+    }
+
+}
